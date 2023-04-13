877897098f18a86f2d33d3631bb014fe56d84d3c
Scott Bailey <Scott@TheBaileyFamily.org>
2023-04-13 10:20:43 -0500

    pylint changes.

--- tools/tests/util.py
+++ tools/tests/util.py
@@ -1,17 +1,18 @@
 #! /usr/bin/env python3
 
+"""Utility functions for antler-proj testing."""
+
 import subprocess
 import os
 import yaml
-import shutil
 
 TEST_PATH = os.path.dirname(os.path.abspath(__file__))
 APROJ_EXE = TEST_PATH + "/../antler-proj"
 
 def run_cmd(cmd):
     """ Execute `cmd`. """
-    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = p.communicate()
+    result = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = result.communicate()
     return out.decode('utf-8'), err.decode('utf-8')
 
 def antler_proj_cmd(args):
@@ -23,18 +24,21 @@ def antler_in_proj_cmd(path, args):
     return run_cmd("cd " + path + "; " + APROJ_EXE + " " + args)
 
 def load_project(path):
-    with open("./{0}/project.yml".format(path), "r") as f:
-        project = yaml.safe_load(f)
+    """ Load a project.yml file """
+    with open("./{0}/project.yml".format(path), "r") as file_handle:
+        project = yaml.safe_load(file_handle)
     return project
 
 def expected_cmd(cmd, expected):
-    out, err = run_cmd(cmd)
+    """ Test that cmd has an expected result. """
+    out, _ = run_cmd(cmd)
     if out != expected:
         print("ERROR: cmd: %s != expected: %s" % (cmd, expected))
-        assert(False)
+        assert False
 
 def expected_proj_cmd(cmd, expected):
-    out, err = antler_proj_cmd(cmd)
+    """ Test that `antler-proj cmd` has an expected result. """
+    out, _ = antler_proj_cmd(cmd)
     if out != expected:
         print("ERROR: cmd: %s != expected: %s" % (cmd, expected))
-        assert(False)
+        assert False
