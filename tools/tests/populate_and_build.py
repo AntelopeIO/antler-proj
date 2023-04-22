1a0a18b8496505da83ba036292b20c3c01b64263
Scott Bailey <Scott@TheBaileyFamily.org>
2023-04-22 12:14:30 -0500

    Add more documentation and debug prints.

--- tools/tests/populate_and_build.py
+++ tools/tests/populate_and_build.py
@@ -16,11 +16,21 @@ SOURCE_PATH=os.path.join("@CMAKE_CURRENT_SOURCE_DIR@/tests",PROJECT_NAME)
 def test_populate_and_build():
     """ Setup a clean project, then populate and build it.
     """
+    # Remove any existing test artifact, then copy the test project over.
     shutil.rmtree(PROJECT_PATH, ignore_errors=True)
     assert shutil.copytree(SOURCE_PATH, PROJECT_PATH)
 
-    subprocess.run([APROJ_EXE,"populate", PROJECT_PATH], check=True)
-    subprocess.run([APROJ_EXE,"build", PROJECT_PATH], check=True)
+    # Populate the project.
+    result = subprocess.run([APROJ_EXE,"populate", PROJECT_PATH], capture_output=True, text=True, check=False)
+    print(result.stdout)
+    print(result.stderr)
+    assert result.returncode == 0
+
+    # Build the project.
+    result = subprocess.run([APROJ_EXE,"build", PROJECT_PATH], capture_output=True, text=True, check=False)
+    print(result.stdout)
+    print(result.stderr)
+    assert result.returncode == 0
 
 
 if __name__ == "__main__":
